6c66b534bbe91b0ef6f660f59268552a98a405df
arirang030 <arirang030@kookmin.ac.kr>
2026-05-09 12:57:03 +0900

    refactor: 마이그레이션 삭제 후 초기 db 재건축

--- backend/src/main/resources/db/migration/V4__add_user_name_birthdate_gender.sql
+++
@@ -1,4 +0,0 @@
--- V4: USER 테이블에 이름(name), 생년월일(birth_date), 성별(gender) 컬럼 추가
-ALTER TABLE `USER` ADD COLUMN name VARCHAR(100) NULL AFTER nickname;
-ALTER TABLE `USER` ADD COLUMN birth_date DATE NULL AFTER student_id;
-ALTER TABLE `USER` ADD COLUMN gender ENUM('MALE','FEMALE','OTHER') NULL AFTER birth_date;
