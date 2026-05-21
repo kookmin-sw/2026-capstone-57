26b0a34e1b673ea520768858a8f175dcdf885d81
Goldbori <shb06114@kookmin.ac.kr>
2026-05-21 21:50:14 +0900

    refactor: 앱 로고 아이콘 변경

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -15,21 +15,9 @@ export const metadata: Metadata = {
   description: '캠퍼스에서 우연히 스친 인연을 발견하세요',
   generator: 'v0.app',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: '/logo.png',
+      
+    apple: '/logo.png',
   },
 }
 
